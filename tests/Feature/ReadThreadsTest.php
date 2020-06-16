006373f62332af143c64892d6722609ed1c3bbc4
Gabriel <gabriel.gressie@gmail.com>
2020-06-16 18:31:04 +0200

    added a popular threads option

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -66,7 +66,22 @@ class ReadThreadsTest extends TestCase
         $this->get('threads?by=JohnDoe')
             ->assertSee($threadByJohn->title)
             ->assertDontSee($threadNotByJohn->title);
+    }
 
+    /** @test */
+    function a_user_can_filter_threads_by_popularity()
+    {
+        // lets make 3 threads - one with 3 replies, one with 2 replies, and one without replies
+        $threadWithThreeReplies = create('App\Thread');
+        create('App\Reply', ['thread_id' => $threadWithThreeReplies->id], 3);
 
+        $threadWithTwoReplies = create('App\Thread');
+        create('App\Reply', ['thread_id' => $threadWithTwoReplies->id], 2);
+
+        $threadWithNoReplies = $this->thread;
+
+        $response = $this->getJson('threads?popular=1')->json();
+
+        $this->assertEquals([3,2,0], array_column($response, 'replies_count'));
     }
 }
