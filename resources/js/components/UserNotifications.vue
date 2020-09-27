c1b13840d106bdccb513a69a898e5d6e90355441
Aljosja84 <gabriel.gressie@gmail.com>
2020-09-27 23:13:53 +0200

    started work on notification UI

--- resources/js/components/UserNotifications.vue
+++ resources/js/components/UserNotifications.vue
@@ -1,10 +1,26 @@
 <template>
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            Notifications
+        </a>
 
+        <ul class="dropdown-menu">
+            <li v-for="notification in notifications">
+                <a href="#">Blue</a>
+            </li>
+        </ul>
+    </li>
 </template>
 
 <script>
     export default {
-        name: "UserNotifications"
+        name: "UserNotifications",
+
+        data() {
+            return {
+                notifications: false
+            }
+        }
     }
 </script>
 
